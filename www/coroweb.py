b03872be8d813a662b850e7ecf2560b398975f47
zhuangda <605079324@qq.com>
2017-08-07 22:32:21 +0800

    add coroweb file

---
+++ www/coroweb.py
@@ -0,0 +1,24 @@
+import functools
+
+
+# 定义一个get装饰器，为装饰的函数添加URL信息
+def get(path):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            return func(*args, **kw)
+        wrapper.__method__ = 'GET'
+        wrapper.__route__ = path
+        return wrapper
+    return decorator
+
+# 定义一个POST装饰器，为装饰的函数添加URL信息
+def post(path):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            return func(*args, **kw)
+        wrapper.__method__ = 'POST'
+        wrapper.__route__ = path
+        return wrapper
+    return decorator
--- www/ormtest.py
+++ www/ormtest.py
@@ -7,8 +7,10 @@ from www.model import User, Blog, Comment
 @asyncio.coroutine
 def test(loop):
     yield from orm.create_pool(loop=loop, user='root', password='123456', db='awesome')
-    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
-    yield from u.save()
+    u = User(name='Test', email='test@example.com', passwd='123456',
+             image='about:blank', admin=False, created_at='1502111285.44569',
+             id='001502111285429762231d51f8e421cb8f4bac381ffd531000')
+    yield from u.remove()
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(test(loop))
